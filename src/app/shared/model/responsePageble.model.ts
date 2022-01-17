cb3f86e4540b4fd12eeaded6dbfd6e01eeb7140f
Ryam Sousa <ryam.sousa123@gmail.com>
2022-01-17 13:25:21 -0300

    create main components and service

---
+++ src/app/shared/model/responsePageble.model.ts
@@ -0,0 +1,12 @@
+export class ResponsePageble {
+  content: any[0];
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageble: any[];
+  size: number;
+  sort: number;
+  totalElements: number;
+  totalPages: number;
+}
