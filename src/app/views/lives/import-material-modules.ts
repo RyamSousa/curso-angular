1c304517b52c2ede244c1daa6b40990c6bdc5d8c
Ryam Sousa <ryam.sousa123@gmail.com>
2022-01-18 16:25:48 -0300

    refactoring material imports

---
+++ src/app/views/lives/import-material-modules.ts
@@ -0,0 +1,27 @@
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+export const MaterialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatDialogModule,
+  MatChipsModule,
+  MatTabsModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatProgressBarModule,
+  MatCardModule
+]
